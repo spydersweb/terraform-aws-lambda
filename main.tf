557a7fb026398f820310de51125a504fdf5b35ec
Graham Cole <graham@sgcole.uk>
2024-05-26 06:15:38 +0100

    more functiony stuff

--- main.tf
+++ main.tf
@@ -1,17 +1,19 @@
-resource "null_resource" "function_binary" {
+# Null resource to build the go binary
+resource "null_resource" "go_function_binary" {
   count = var.build_go_binary ? 1 : 0
   provisioner "local-exec" {
-    command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ${path.module}/tf_generated/bootstrap ${local.src_path}"
+    command = "GOOS=linux GOARCH=amd64 CGO_ENABLED=0 GOFLAGS=-trimpath go build -mod=readonly -ldflags='-s -w' -o ${local.go_output_file} ${local.src_path}"
   }
 }
 
-data "archive_file" "function_archive" {
+# Archive file resource to zip the created binary
+data "archive_file" "go_function_archive" {
   count = var.build_go_binary ? 1 : 0
 
-  depends_on = [null_resource.function_binary]
+  depends_on = [null_resource.go_function_binary]
 
   type        = "zip"
-  source_file = "${local.binary_path}/${local.binary_name}"
+  source_file = "${local.go_output_file}"
   output_path = local.archive_path
 }
 
@@ -28,8 +30,11 @@ resource "aws_lambda_function" "function" {
   runtime = var.runtime
 
   timeout = var.timeout
+
+  tags = var.function_tags
 }
 
+# Cloudwatch logging group
 resource "aws_cloudwatch_log_group" "log_group" {
   name              = "/aws/lambda/${aws_lambda_function.function.function_name}"
   retention_in_days = 7
