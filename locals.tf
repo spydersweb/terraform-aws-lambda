7db259db8df2a595efa0f87a5090d3b4fd55f4ac
Graham Cole <graham@sgcole.uk>
2024-06-01 06:06:43 +0100

    project switch wip

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
 locals {
   archive_path   = "${path.module}/tf_generated/${local.runtime_name}.zip"
-  binary_path    = "${path.module}/tf_generated" #${path.module}/tf_generated/bootstrap
+  binary_path    = "${path.module}/tf_generated"
   go_output_file = "${path.module}/tf_generated/${local.runtime_name}"
   runtime_name   = "bootstrap"
-  src_path       = "github.com/spydersweb/aws/lambda/${var.function_name}"
+  src_path       = "${var.lambda_source_path}/${var.function_name}"
 }
 
